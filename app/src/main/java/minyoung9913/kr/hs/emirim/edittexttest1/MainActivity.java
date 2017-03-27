dfdd61fd05ae809cd4613235de14105e8978a6d1
mykimn <min0905@e-mirim.hs.kr>
2017-03-27 19:08:30 +0900

    #2 Java Event complete

--- app/src/main/java/minyoung9913/kr/hs/emirim/edittexttest1/MainActivity.java
+++ app/src/main/java/minyoung9913/kr/hs/emirim/edittexttest1/MainActivity.java
@@ -1,13 +1,30 @@
 package minyoung9913.kr.hs.emirim.edittexttest1;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edit1;
+    TextView textResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit1 = (EditText)findViewById(R.id.edit1);
+        Button butOk = (Button)findViewById(R.id.but_ok);
+        textResult = (TextView)findViewById(R.id.text_result);
+
+        butOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = edit1.getText().toString();
+                textResult.setText("입학한 성명은 "+name+"입니다.");
+            }
+        });
     }
 }
